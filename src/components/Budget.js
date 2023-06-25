0db0d97d0c92fec7e970baf4291dfeb9e4ed55e9
jonasbetfiang <jonasbetfiang@yahoo.fr>
2023-06-25 19:20:12 -0400

    Changing React components
    Signed-off-by: jonasbetfiang <jonasbetfiang@yahoo.fr>

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,46 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { budget, expenses, dispatch, currency } = useContext(AppContext);
+    const [currentBudget, setCurrentBudget] = useState(budget);
+
+    const updateCurrentBudget = (event) => {   
+        let totalSpent = expenses.reduce((total, currentExp) => {
+            total = total + currentExp.cost;
+            return total;
+        }, 0);
+
+        if(event.target.value > 20000) {
+            alert("Budget cannot exceed 20000");
+        }
+        else if (event.target.value < totalSpent){
+            alert("You cannot reduce the budget value lower than the spending");
+        }
+        else {
+            setCurrentBudget(event.target.value);
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: event.target.value
+            });
+            console.log(budget);
+        }
+    }
+
+    return (
+        <div className='alert alert-secondary'>
+            <span style={{fontSize:16}}>Budget: {currency}
+            <input 
+                type='number' 
+                step='10'
+                id='budgetInput'
+                name='budgetInput'
+                value={currentBudget}
+                style={{size: 10}}
+                onChange={(event) => updateCurrentBudget(event)}
+            />
+            </span>
+        </div>
+    );
+};
+export default Budget;
